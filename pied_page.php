774d80e2b26aeb2dd3e4ef7fd91431e423c39c88
SCDI Montpellier <58264175+scdi-montpellier@users.noreply.github.com>
2023-01-05 14:10:35 +0100

    Add files via upload

--- pied_page.php
+++ pied_page.php
@@ -4,7 +4,7 @@
 	
 	echo "<footer class='footer'>";
     echo "<div class='container'>";
-    echo "<span class='text-muted'><img style='height:48px;' src='$gURLlogo' alt='Logo' title='Logo'> <a class='text-decoration-none' target='_blank' alt='Accéder au site du $gNominstitutionel' title='Accéder au site du $gNominstitutionel' href='$gURLsiteinstitutionel'>$gNominstitutionel</a></span>";
+    echo "<span class='text-muted'><img style='height:48px;' src='$gURLlogo' alt='Logo' title='Logo'> <a class='text-decoration-none' target='_blank' alt='Accéder au site du $gNominstitutionel' title='Accéder au site du $gNominstitutionel' href='$gURLsiteinstitutionel'>$gNominstitutionel</a></span> <span style='white-space: nowrap;'>| $gNomAppliCourt - v$gVersionAppli</span>";
     echo "</div>";
     echo "</footer>";
 
